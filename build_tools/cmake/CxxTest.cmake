2c64c8e552d9dbcae458e79be3f1201f8b34a4bb
Gašper <gasper.azman@gmail.com>
2012-03-13 12:07:22 +0100

    Another try at the cmake builder.

--- build_tools/cmake/CxxTest.cmake
+++ build_tools/cmake/CxxTest.cmake
@@ -1,15 +1,24 @@
-include(FindCxxTest.cmake)
+FIND_PROGRAM(CXXTESTGEN
+    NAMES cxxtestgen
+    PATHS ./cxxtest/bin/
+          /usr/bin
+          /bin
+          )
 
-function(cxx_test target source)
-    string(REGEX REPLACE "hpp$" "cpp" CPP_FILE_NAME ${source})
-    message(${CPP_FILE_NAME})
-    set(CPP_FULL_NAME "${CMAKE_CURRENT_BINARY_DIR}/${CPP_FILE_NAME}")
-    add_custom_command(
+IF(NOT CXXTESTGEN)
+    MESSAGE(FATAL_ERROR "Unable to find 'cxxtestgen'")
+ENDIF(NOT CXXTESTGEN)
+
+FUNCTION(cxx_test target source)
+    STRING(REGEX REPLACE "hpp$" "cpp" CPP_FILE_NAME ${source})
+    MESSAGE(${CPP_FILE_NAME})
+    SET(CPP_FULL_NAME "${CMAKE_CURRENT_BINARY_DIR}/${CPP_FILE_NAME}")
+    ADD_CUSTOM_COMMAND(
         OUTPUT "${CPP_FULL_NAME}"
         COMMAND ${CXXTESTGEN} --runner=ErrorPrinter --output "${CPP_FULL_NAME}" "${source}"
         DEPENDS "${source}"
     )
-    add_executable(${target} ${CPP_FULL_NAME})
-    set_target_properties(${target} PROPERTIES COMPILE_FLAGS "-Wno-effc++")
-    add_test(${target} ${RUNTIME_OUTPUT_DIRECTORY}/${target})
-endfunction(cxx_test)
+    ADD_EXECUTABLE(${target} ${CPP_FULL_NAME})
+    SET_TARGET_PROPERTIES(${target} PROPERTIES COMPILE_FLAGS "-Wno-effc++")
+    ADD_TEST(${target} ${RUNTIME_OUTPUT_DIRECTORY}/${target})
+ENDFUNCTION(cxx_test)
